41d5121b773aaebe45faecabd0dfcfc61ad97647
wahidx <wahidx93@gmail.com>
2023-10-12 16:53:30 +0530

    added chi and middleware along with it

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"go-gql/todox"
 	"go-gql/todox/resolvers"
 	"go-gql/todox/store"
@@ -10,6 +11,7 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi/v5"
 )
 
 const defaultPort = "8080"
@@ -20,17 +22,32 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(todox.NewExecutableSchema(
-		todox.Config{
-			Resolvers: &resolvers.Resolver{
-				Store: store.New(),
-			},
-		},
-	))
+	srv := handler.NewDefaultServer(todox.NewExecutableSchema(todox.Config{
+		Resolvers: &resolvers.Resolver{Store: store.New()},
+	}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router := chi.NewRouter()
+	router.Use(auth())
+
+	router.Handle("/", playground.Handler("TodoX Playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
+
+func auth() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			token := r.Header.Get("auth-token")
+			fmt.Println("AUTH_TOKEN in MW: ", token)
+
+			if len(token) == 0 || token != "secrettoken" {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
 }
